ec2e4903802b3bed3e90ba46947784f68cf0d49d
mart6110 <martinus.mortensen@gmail.com>
2023-04-17 13:48:05 +0200

    Add project files.

---
+++ Tråd2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+
+namespace Tråd2
+{
+    class Program
+    {
+        static char ch = '*';
+        static void Main(string[] args)
+        {
+
+            Thread p = new Thread(output);
+            p.Name = "Printer";
+            p.Start();
+
+            Thread r = new Thread(input);
+            r.Name = "Reader";
+            r.Start();
+
+            p.Join();
+            r.Join();
+
+            Console.ReadKey();
+
+
+            static void input()
+            {
+                while (true)
+                {
+                    ch = Console.ReadKey().KeyChar;
+                }
+            }
+
+            static void output()
+            {
+                while (true)
+                {
+                    Thread.Sleep(200);
+                    Console.Write(ch);
+                }
+            }
+
+        }
+    }
+}
